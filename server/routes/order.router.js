8024c73b282e51216fb17065b70b2a42e93bbac9
lukemm23 <lukemm23@gmail.com>
2020-01-13 14:23:31 -0600

    created DELETE order, PUT order end points

--- server/routes/order.router.js
+++ server/routes/order.router.js
@@ -32,5 +32,45 @@ router.post('/', (req, res, next) => {
       .catch(() => res.sendStatus(500));
   });
 
+   /**
+ * DELETE route template, DELETE customer from database
+ */
+router.delete('/:id', (req, res) => {
+    const queryText = `DELETE FROM "orders"
+                        WHERE "id" = $1;`;
+
+    pool.query(queryText, [req.params.id])
+        .then((response) => {
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+});
+
+/**
+ * PUT route template, PUT updates order information into database
+ */
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    console.log(req.body.service);
+    console.log(id);
+
+    let queryString = `UPDATE "orders" SET estimate_time='${data.estimate_time}',service='${data.service}',
+    service_frequency='${data.service_frequency}', service_due='${data.service_due}',tax_due='${data.tax_due}', total_due='${data.total_due}',status='${data.status}'
+    WHERE "id" = $1;`;
+
+    pool.query(queryString, [id])
+        .then((response) => {
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        })
+});
+
 module.exports = router;
 
